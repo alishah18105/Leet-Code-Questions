a10057efed032ada1de40ec11df438356025baf9
alishah18105 <alishah18105@gmail.com>
2026-02-01 20:01:48 +0500

    Leetcode String Question 1-02-2026

--- String/leetcode4.py
+++ String/leetcode4.py
@@ -1,10 +1,5 @@
 haystack = "leetcode"
 needle = "leet0"
 
-if needle in haystack:
-    print("True")
-else:
-    print("False")
-
-ans = haystack.find(needle)
-print(ans)
+index = haystack.find(needle)
+print(index)
